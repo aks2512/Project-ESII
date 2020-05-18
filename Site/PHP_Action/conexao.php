46c6f78957a934280faaa5dddb62f3ff0353c054
aks2512 <jefferson_akira@hotmail.com>
2020-05-18 18:10:17 -0300

    upt conexao.php

--- Site/PHP_Action/conexao.php
+++ Site/PHP_Action/conexao.php
@@ -2,6 +2,7 @@
 
    // Create connection
    $conn = mysqli_connect("localhost", "root", "", "transparenciamc");
+    mysqli_set_charset($conn, 'utf8');
    // Check connection
    if (!$conn) {
        die("Falha na conexao: " . mysqli_connect_error());
