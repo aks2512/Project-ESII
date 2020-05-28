d518f6251edad9d9da04735ab51a5bc3eeda95b0
aks2512 <jefferson_akira@hotmail.com>
2020-05-28 16:15:57 -0300

    upt menu principal

--- Site/database/transparenciamc.sql
+++ Site/database/transparenciamc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19-Maio-2020 às 00:18
+-- Tempo de geração: 27-Maio-2020 às 21:48
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.4.5
 
@@ -40,7 +40,8 @@ CREATE TABLE `administradores` (
 INSERT INTO `administradores` (`id_admin`, `Senha`, `Usuario`) VALUES
 (1, '202cb962ac59075b964b07152d234b70', 'William'),
 (2, '202cb962ac59075b964b07152d234b70', 'asdsad'),
-(3, '2b0c8408ea77ba5fe4b4b72cae5d0146', 'Akira');
+(3, '2b0c8408ea77ba5fe4b4b72cae5d0146', 'Akira'),
+(4, '202cb962ac59075b964b07152d234b70', 'sadasd');
 
 -- --------------------------------------------------------
 
@@ -61,11 +62,15 @@ CREATE TABLE `detalhes` (
 --
 
 INSERT INTO `detalhes` (`id`, `id_item`, `categoria`, `subcategoria`, `valor`) VALUES
-(2, 1, 'Remuneração', 'Hora Extra', 589.45),
-(2, 2, 'Remuneração', 'Honorarios', 987.76),
-(2, 3, 'Remuneração', 'Salario', 3000.67),
-(2, 4, 'Desconto', 'Imp. de Renda', 253),
-(2, 5, 'Desconto', 'IPREM', 1314.9);
+(2, 10, 'Remuneracao', 'Bonus', 1788.45),
+(2, 11, 'Remuneracao', 'Salario', 2789.45),
+(2, 12, 'Desconto', 'Imp. de Renda', 233.12),
+(2, 13, 'Desconto', 'IPREM', 589.33),
+(3, 14, 'Remuneracao', 'Hora extra', 345.78),
+(3, 15, 'Remuneracao', 'Bonus', 2789.45),
+(3, 16, 'Remuneracao', 'Salario', 5673.43),
+(3, 17, 'Desconto', 'Imp. de Renda', 233.12),
+(3, 18, 'Desconto', 'IPREM', 589.33);
 
 -- --------------------------------------------------------
 
@@ -77,7 +82,6 @@ CREATE TABLE `funcionarios_bd` (
   `id` int(255) NOT NULL,
   `Nome` varchar(255) NOT NULL,
   `Cargo` varchar(255) NOT NULL,
-  `Remuneracao` varchar(255) NOT NULL,
   `Modificado` varchar(255) NOT NULL,
   `Regime` varchar(255) NOT NULL,
   `TBruto` varchar(255) NOT NULL,
@@ -91,8 +95,9 @@ CREATE TABLE `funcionarios_bd` (
 -- Extraindo dados da tabela `funcionarios_bd`
 --
 
-INSERT INTO `funcionarios_bd` (`id`, `Nome`, `Cargo`, `Remuneracao`, `Modificado`, `Regime`, `TBruto`, `Tliquido`, `TDescontos`, `DescontosObgr`, `OutrosDescontos`) VALUES
-(2, 'Ivenspreison', 'Gerente Genérico', '4577.88', '27/13/2200', 'AGE', '4000.56', '3500.7', '4690.9', '1567.9', '3123');
+INSERT INTO `funcionarios_bd` (`id`, `Nome`, `Cargo`, `Modificado`, `Regime`, `TBruto`, `Tliquido`, `TDescontos`, `DescontosObgr`, `OutrosDescontos`) VALUES
+(2, 'Leonardo', 'Tecnico em informatica', '30/07/2020', 'INSS', '4577.9', '3720.79', '857.11', '822.45', '34.66'),
+(3, 'José', 'Analista de Sistemas', '24/05/2018', 'INSS', '8808.66', '7652.88', '1155.78', '822.45', '333.33');
 
 --
 -- Índices para tabelas despejadas
@@ -125,19 +130,19 @@ ALTER TABLE `funcionarios_bd`
 -- AUTO_INCREMENT de tabela `administradores`
 --
 ALTER TABLE `administradores`
-  MODIFY `id_admin` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_admin` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `detalhes`
 --
 ALTER TABLE `detalhes`
-  MODIFY `id_item` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_item` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de tabela `funcionarios_bd`
 --
 ALTER TABLE `funcionarios_bd`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restrições para despejos de tabelas
